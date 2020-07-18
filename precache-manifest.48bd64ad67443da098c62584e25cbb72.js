20c6c57728ee231508a500d0ec9519cb5412133a
Michał Adamczyk <hello@codeman.it>
2020-07-18 19:46:53 +0200

    Updates

---
+++ precache-manifest.48bd64ad67443da098c62584e25cbb72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49711185383f3e3d725846b87dc4a22c",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "39e66eb2eb8dacbb6057",
+    "url": "/todo-list-react/static/css/main.fc5d9f53.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e66eb2eb8dacbb6057",
+    "url": "/todo-list-react/static/js/main.fad1a040.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
