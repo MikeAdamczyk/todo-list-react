d77fc6cd4261e780771fc3d49705c3a5a7444d68
Michał Adamczyk <hello@codeman.it>
2020-07-20 17:35:04 +0200

    toggleHideDone interaction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -14,9 +14,14 @@ const tasks = [
   {id: 3, content: "pobiegać min. 30 min wieczorem", done: false},
 ];
 
-const hideDone = false;
-
 function App() {
+
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone)
+  };
+
   return (
     <Container>
 
@@ -29,7 +34,13 @@ function App() {
 
         <SectionDisplayTasks
             title="Lista zadań"
-            extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone}/>}
+            extraHeaderContent={
+            <Buttons
+              tasks={tasks}
+              hideDone={hideDone}
+              toggleHideDone={toggleHideDone}
+            />
+            }
             body={<Tasks tasks={tasks} hideDone={hideDone}/>}
         />
 
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css"
 
-const Buttons = ({tasks, hideDone}) => (
+const Buttons = ({tasks, hideDone, toggleHideDone}) => (
 
     tasks.length > 0 && (
         <span className="displayTasks__buttonBox">
             <button
+              onClick={toggleHideDone}
               className="displayTasks__controlButtons"
               disabled={tasks.every(({done}) => !done)}
             >
