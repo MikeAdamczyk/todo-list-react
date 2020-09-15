e957479649107c72aa311bef04c6e54c2dbff5cf
Michał Adamczyk <hello@codeman.it>
2020-09-15 12:34:31 +0200

    add colors in theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,9 @@
 export const theme = {
     colors: {
         backgroundColor: "white",
+        teal: "#008080",
+        lightGrey: "#ccc",
+        concrete: "#F2F2F2",
     },
 
     breakpoints: {
