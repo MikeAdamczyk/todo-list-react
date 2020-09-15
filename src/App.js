e56761330486e2658792ec91f4bfb7a5c52264ac
Michał Adamczyk <hello@codeman.it>
2020-09-15 11:53:40 +0200

    delete ThemeProvider from App

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import SectionDisplayTasks from "./SectionDisplayTasks";
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "./Container";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
 import { useTasks } from "./useTasks";
 
 function App() {
@@ -27,43 +25,39 @@ function App() {
   } = useTasks();
 
   return (
-    <ThemeProvider theme={theme}>
+    <Container>
 
-      <Container>
+      <Header title="Lista zadań" />
 
-        <Header title="Lista zadań" />
+      <SectionAddTask
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
 
-        <SectionAddTask
-          title="Dodaj nowe zadanie"
-          body={<Form addNewTask={addNewTask} />}
-        />
+      <SectionDisplayTasks
+        title="Lista zadań"
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
 
-        <SectionDisplayTasks
-          title="Lista zadań"
-          extraHeaderContent={
-            <Buttons
-              tasks={tasks}
-              hideDone={hideDone}
-              toggleHideDone={toggleHideDone}
-              setAllDone={setAllDone}
-            />
-          }
-          body={
-            <Tasks
-              tasks={tasks}
-              hideDone={hideDone}
-              removeTask={removeTask}
-              toggleTaskDone={toggleTaskDone}
+          />
+        }
+      />
 
-            />
-          }
-        />
+      <Footer title="© Michał Adamczyk 2020. Wszystkie prawa zastrzeżone!" />
 
-        <Footer title="© Michał Adamczyk 2020. Wszystkie prawa zastrzeżone!" />
-
-      </Container>
-
-    </ThemeProvider>
+    </Container>
   );
 };
 
