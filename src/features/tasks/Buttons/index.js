572fcee4e5d4ae7e3ded2e6e406f52554edd04b3
Michał Adamczyk <hello@codeman.it>
2020-09-30 14:12:27 +0200

    refactor

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import { Wrapper } from "./styled";
 import { ControlButton } from "../ControlButtons";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone, setAllDone, selectHideDone, selectIsEveryTaskDone } from "../tasksSlice";
+import {
+  selectTasks,
+  toggleHideDone,
+  setAllDone,
+  selectHideDone,
+  selectIsEveryTaskDone,
+  selectIsTaskListEmpty
+} from "../tasksSlice";
 
 const Buttons = () => {
 
   const tasks = useSelector(selectTasks);
   const hideDone = useSelector(selectHideDone);
   const everyTaskDone = useSelector(selectIsEveryTaskDone);
+  const isTaskListEmpty = useSelector(selectIsTaskListEmpty);
   const dispatch = useDispatch();
 
   return (
-    tasks.length > 0 && (
+    !isTaskListEmpty > 0 && (
       <Wrapper>
         <ControlButton
           onClick={() => dispatch(toggleHideDone())}
